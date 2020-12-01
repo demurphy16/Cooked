73d7aa0cab254b4bc73c358117cfd4b6dcc3ed3a
David Murphy <demurphy16@brandeis.edu>
2020-12-01 16:23:04 -0500

    added more functionality to login

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,32 @@
-import { Switch, Route } from 'react-router-dom'
+import {useState} from 'react'
+import { Switch, Route, useHistory } from 'react-router-dom'
 
 import './App.css';
 import Layout from './layouts/layout';
 import Login from './screens/Login'
+import { loginUser } from './services/auth'
 
 function App() {
+
+  const [currentUser, setCurrentUser] = useState(null)
+  const history = useHistory()
+
+
+  const handleLogin = async (loginData) => {
+    const userData = await loginUser(loginData)
+    setCurrentUser(userData)
+    history.push('/')
+  }
+
   return (
       <Layout>
       <Switch>
         
           <Route path='/login'>
           <h3>Login</h3>
-          <Login />
+          <Login
+          handleLogin={handleLogin}
+          />
           </Route>
         
           <Route path='/register'>
